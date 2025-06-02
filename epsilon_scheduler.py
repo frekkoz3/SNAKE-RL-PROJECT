10ebbf5c8c0e41bc3155f4e748bbea852a27c605
Rubentic <rubentic2003@gmail.com>
2025-06-02 11:30:42 +0200

    Update epsilon_scheduler.py

--- epsilon_scheduler.py
+++ epsilon_scheduler.py
@@ -8,6 +8,8 @@ class Epsilon:
         """
             This is just an Epsilon schedule. In fact it is the constant one
         """
+
+        assert 0 < eps < 1, "Error in Epsilon: eps must be a probability (between 0 and 1)"
         self.first_eps = eps
         self.eps = eps
     
@@ -26,19 +28,34 @@ class ConstantEpsilonDecay(Epsilon):
 
 class LinearEpsilonDecay(Epsilon):
 
-    def __init__(self, eps, coefficient = 0.999, minimum = 0.1 ):
+    def __init__(self, eps, coefficient = 0.999, minimum = 0.1, n_steps_to_minimum=None):
         """
             This is the linear epsilon decay.
             the decay works as follows:
-                eps = max(minimum, eps*coef)
+                if steps_to_minimum is provided --> eps decays exponentially reaching the minimum in n_steps_to_minimum steps
+                else                            --> eps = max(minimum, eps*coef)
         """
         super().__init__(eps)
-        self.coefficient = coefficient
+        assert  0 <= minimum <= 1 , "Error in LinearEpsilonDecay: minimum must be a probabilty (between 0 and 1)"
         self.minimum = minimum
+        self.n_steps_to_minimum = n_steps_to_minimum
+        if n_steps_to_minimum is None:
+            assert 0 < coefficient < 1, "Error in LinearEpsilonDecay: coefficient must be between 0 and 1" 
+            self.coefficient = coefficient
+        else:
+            assert n_steps_to_minimum > 0, "Error in LinearEpsilonDecay: number of steps for minimum must be stictly positive"
+            self.coefficient = (self.minimum/self.first_eps)**(1/n_steps_to_minimum)
+        
     
     def decay(self):
         self.eps = max(self.minimum, self.eps*self.coefficient)
         return self.eps
     
     def __str__(self):
-        return f"Linear Epsilon eps0 {self.first_eps} coef {self.coefficient} min {self.minimum}"
+        if self.n_steps_to_minimum is None:
+            return f"Linear Epsilon eps0 {self.first_eps} coef {self.coefficient} min {self.minimum}"
+        else:
+            return f"Linear Epsilon eps0 {self.first_eps} min {self.minimum} reached in {self.n_steps_to_minimum} steps"
+
+
+

316650dbea83fb891b8bd40c5d842d0df9094aa1
FRANCESCO BREDARIOL <francescobredariol03@gmail.com>
2025-06-03 12:00:02 +0200

    updated the type of thoughts, then changer a little comment.

--- eligibility_traces.py
+++ eligibility_traces.py
@@ -1,12 +1,14 @@
 """
-The eligibility_traces.py module provides functions to manage eligibility traces in the Snake project.
+    This work is developed for academic purpose by Bredariol Francesco, Savorgnan Enrico, Tic Ruben.
+    This work is part of the final project for the 2024-2025 Reinforcement Learning course at the University of Trieste.
 """
 from collections import defaultdict
 
 
 class Eligibility:
     """
-    This class implements the eligibility traces mechanism for TD(λ) algorithms.
+        The eligibility_traces.py module provides functions to manage eligibility traces in the Snake project.
+        This class implements the eligibility traces mechanism for TD(λ) algorithms.
     """
     def __init__(self, lambda_value, gamma=0.9):
         self.lambda_value = lambda_value
@@ -15,13 +17,13 @@ class Eligibility:
 
     def reset(self):
         """
-        Reset the eligibility traces.
+            Reset the eligibility traces.
         """
         self.traces.clear()
 
     def update(self, state_action):
         """
-        state_action here is in the usual format (*state, action)
+            state_action here is in the usual format (*state, action)
         """
         self.traces[state_action] += 1.0
 
